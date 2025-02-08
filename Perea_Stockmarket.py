b832310505cde50039323767bd0e03283870cf37
Noah Perea <noahperea323@gmail.com>
2025-02-07 21:06:57 -0700

    Added try and except block incase data fetch fails.

--- Perea_Stockmarket.py
+++ Perea_Stockmarket.py
@@ -16,11 +16,14 @@ def download_data(ticker: str)-> dict:
     path = f"/api/quote/{ticker}/historical?assetclass=stocks&fromdate={start}&limit=9999"
     URL = base_url + path
     headers = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
-    response = get((URL), headers=headers)
-    raw_data = response.json()
-    data = sorted(raw_data['data']["tradesTable"]["rows"], key = lambda x: x['date'])
-    data_dict = {item["date"]: item for item in data}
-    return data_dict
+    try:
+        response = get((URL), headers=headers)
+        raw_data = response.json()
+        data = sorted(raw_data['data']["tradesTable"]["rows"], key = lambda x: x['date'])
+        data_dict = {item["date"]: item for item in data}
+        return data_dict
+    except Exception as e:
+        print(e)
 
 def avg(lst: list)-> float:
     lsum = 0
