b650c33d4368614872a3ea50c974abe2e7006d33
Noah Perea <noahperea323@gmail.com>
2025-02-07 20:55:18 -0700

    Completed stage 2 of the assignment, data processing function has been completed.

--- Perea_Stockmarket.py
+++ Perea_Stockmarket.py
@@ -22,8 +22,32 @@ def download_data(ticker: str)-> dict:
     data_dict = {item["date"]: item for item in data}
     return data_dict
 
-    
+def avg(lst: list)-> float:
+    lsum = 0
+    for i in lst:
+        lsum += i
+    fsum = lsum / len(lst)
+    return fsum
+
+def median(lst:list)-> float:
+    ordered_lst = sorted(lst)
+    med_item = int(len(lst) / 2)
+    med_val = ordered_lst[med_item]
+    return med_val
+
+def data_processing(data_dict: dict) -> dict:
+    closing_data = []
+    for key, item in data_dict.items():
+        closing_value = item.get("close")
+        removed_str = closing_value.replace("$","").strip()
+        closing_data.append(float(removed_str))
+    max_close = max(closing_data)
+    min_close = min(closing_data)
+    avg_close = avg(closing_data)
+    median_close = median(closing_data)
+    Output_dict = {"Min: ": min_close, "Max: ": max_close, "Avg: ": avg_close, "Median: ": median_close}
+    return Output_dict
 
 
-
-print(download_data("AAPL"))
+lst = data_processing(download_data("AAPL"))
+print(lst)
